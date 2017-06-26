aba22bdba3a1982261e6283e2097415972890119
George <bumgardnera07@gmail.com>
2017-06-25 21:24:46 -0400

    checkpoint 8 finished

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,15 +1,15 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
       /**
       * @desc object returned by the songplayer service for public use
       * @type {Object}
       */
       var SongPlayer = {};
       /**
-      * @desc current song object from album songs array
+      * @desc gets current album information for the playing song
       * @type {Object}
       */
-      var currentSong = null;
+      var currentAlbum = Fixtures.getAlbum();
       /**
       * @desc Buzz object audio file
       * @type {Object}
@@ -19,7 +19,7 @@
       * @function playSong
       * @desc Plays currently scoped song and sets playing property to true
       */
-      var playSong = function() {
+      var playSong = function(song) {
         currentBuzzObject.play();
         song.playing = true;
       }
@@ -31,7 +31,7 @@
       var setSong = function(song) {
           if (currentBuzzObject) {
             currentBuzzObject.stop();
-            currentSong.playing = null;
+            SongPlayer.currentSong.playing = null;
           }
 
           currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -39,38 +39,69 @@
             preload: true
           });
 
-          currentSong = song;
+          SongPlayer.currentSong = song;
           };
-
+          /**
+          * @function getSongIndex
+          * @desc Retrieves songIndex from current song albumData
+          * @param {Object} song
+          */
+          var getSongIndex = function(song) {
+              return currentAlbum.songs.indexOf(song);
+          };
+          /**
+          * @desc current song object from album songs array
+          * @type {Object}
+          */
+          SongPlayer.currentSong = null;
           /**
           * @function .play
           * @desc public method for switching songs and playing songs when none are playing
           * @param {Object} song
           */
           SongPlayer.play = function(song) {
-           if (currentSong !== song) {
+          song = song || SongPlayer.currentSong;
+           if (SongPlayer.currentSong !== song) {
              setSong(song);
-             playSong();
-        } else if (currentSong === song) {
-          if (currentBuzzObject.isPaused()) {
-          playSong();
+             playSong(song);
+           } else if (SongPlayer.currentSong === song) {
+             if (currentBuzzObject.isPaused()) {
+               playSong(song);
+          }
         }
-      }
-   };
+    };
    /**
    * @function .pause
    * @desc public method for pausing the song
    * @param {Object} song
    */
-   SongPlayer.pause = function(song) {
-    currentBuzzObject.pause();
-    song.playing = false;
-};
+    SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
+      currentBuzzObject.pause();
+      song.playing = false;
+    };
+    /**
+    * @function .previous
+    * @desc decrements the currentSongIndex value
+    */
+    SongPlayer.previous = function() {
+        var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+        currentSongIndex--;
+
+        if (currentSongIndex < 0) {
+          currentBuzzObject.stop();
+          SongPlayer.currentSong.playing = null;
+        } else {
+          var song = currentAlbum.songs[currentSongIndex];
+          setSong(song);
+          playSong(song);
+          }
+    };
 
          return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
